0920303a435b9b46307a0fad519206311838bb52
blak-kong <546598185@qq.com>
2018-06-10 22:14:53 +0800

    swiper

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">
+        <i class="icon-fanhui"></i>
+      </span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">
+        <i class="icon-sousuo"></i>
+      </span>
+      输入城市/景点/游玩主题</div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">
+        <i class="icon-jiantou"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor /*抽离公用样式*/
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      display: flex
+      justify-content: center
+      .back-icon
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem /*86-24(margin)*/
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
+</style>
---
+++ src/pages/home/components/HomeSwiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+    <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination', //下标点
+        loop: true // 轮播
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/3b/ef86879aa50e3002.jpg_750x200_2a108508.jpg'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/e5/59fad13a64807d02.jpg_750x200_713ae984.jpg'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/13/6332699c44387902.jpg_750x200_b7afa4f8.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+/*样式穿透scoped*/
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff!important
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0 /*高度相对宽度自动撑开31.24%*/
+  padding-bottom: 26.66%
+  background: #eee /*图片没显示出来时显示*/
+  .swiper-img
+    width: 100%
+</style>
